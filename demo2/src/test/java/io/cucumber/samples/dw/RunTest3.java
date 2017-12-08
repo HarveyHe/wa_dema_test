9a7327b88fc8128d8884b932e530c1a40558da49
Harvey.He <Harvey.He@GSSTHARVEY.gsst.com>
2017-12-08 14:33:58 +0800

    t

--- demo2/src/test/java/io/cucumber/samples/dw/RunTest3.java
+++ demo2/src/test/java/io/cucumber/samples/dw/RunTest3.java
@@ -3,7 +3,7 @@ package io.cucumber.samples.dw;
 public class RunTest3 {
 	@org.junit.Before
 	public void test(){
-		System.out.println("sssssssss");
+		System.out.println("sssssstttsss");
 	}
 	
 }
